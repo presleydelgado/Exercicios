7e83acb9586f0201ddcc92865764713798301ce0
Presley Bonifacio Delgado <presley.bonifacio@gmail.com>
2023-10-11 20:04:22 -0300

    SINAL ERRADOOOOOOOOO

--- IMC/Program.cs
+++ IMC/Program.cs
@@ -1,37 +1,34 @@
-﻿var masculino = "true" ;
-var feminino = "false";
-double altura = 0;
+﻿double altura;
+string sexo;
 
-
-/* calculo  masculino */
-
-double valorm1 = 72.7;
-double valorm2 = -58;
-double massam1 = (valorm1 * altura) - valorm2;
-
-/* calculo feminino */
-
-double valorf1 = 62.1;
-double valorf2 = -44.7;
-double massaf1 = (valorf1 * altura) - valorf2;
 
 try { 
 Console.WriteLine("Qual o seu sexo ?");
 
-masculino = Console.ReadLine();
+sexo = Console.ReadLine();
 
 
 Console.WriteLine("Qual a sua altura ?");
 
 altura = double.Parse(Console.ReadLine());
 
-    if (true)
+    if (sexo == "masculino")
 {
-    Console.WriteLine("Para o seu sexo masculino o seu peso ideal é de : " + massam1 + " KG" );
+        double valorm1 = 72.7;
+        double valorm2 = 58;
+        double valorm3 = (valorm1 * altura); 
+        double valorm4 = (valorm3 - valorm2);
+
+        Console.WriteLine("Para o seu sexo masculino o seu peso ideal é de : " + valorm4 + " KG" );
 }
-    if (false)
+    if (sexo == "feminino")
 {
-    Console.WriteLine("Para o seu sexo feminino o seu peso ideal é de : " + massaf1 + " KG");
+
+        double valorf1 = 62.1;
+        double valorf2 = -44.7;
+        double massaf1 = (valorf1 * altura) - valorf2;
+
+        Console.WriteLine("Para o seu sexo feminino o seu peso ideal é de : " + massaf1 + " KG");
 }
  }
 catch
